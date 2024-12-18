51fd2f7e714d6da0339c1219f8c694e02b52d4af
RadivanHanivansyah <radivan100@gmail.com>
2024-12-18 18:50:14 +0700

    Membuat navbar versi dekstop

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const humberger = document.querySelector('#humberger');
+const navMenu = document.querySelector('#nav-menu');
 humberger.addEventListener('click', function () {
     humberger.classList.toggle('humberger-active');
+    navMenu.classList.toggle('hidden');
 });
 
 window.onscroll = function () {
